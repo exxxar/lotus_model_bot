b2e4f47c8a4619842a099f5df6066e668ee77413
exxxar <exxxar@gmail.com>
2020-01-06 14:45:25 +0300

    обновил контент бота

--- app/Profile.php
+++ app/Profile.php
@@ -7,4 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     //
+
+    protected $fillable = [
+        'full_name',
+        'height',
+        'weight',
+        'breast_volume',
+        'sex',
+        'waist',
+        'hips',
+        'model_school_education',
+        'about',
+        'hobby',
+        'education',
+        'wish_learn',
+        'age',
+        'clothing_size',
+        'shoe_size',
+        'eye_color',
+        'hair_color',
+
+
+        'city',
+    ];
 }
--- app/User.php
+++ app/User.php
@@ -16,7 +16,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'telegram_chat_id',
+        'name_from_telegram',
+        'phone',
+        'is_admin'
     ];
 
     /**
